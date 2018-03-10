d14e8fb10093ae0dbb45143f352b561f5a7cfb42
yangbin130 <yangbin15@jd.com>
2018-03-10 21:31:19 +0800

    Revert "Revert "fjl""
    This reverts commit 5efbc8e97462530f39d7adf79d6eec7ac4f9c578.

--- SelectedWord_Demo/SelectedWord_Demo/ViewController.m
+++ SelectedWord_Demo/SelectedWord_Demo/ViewController.m
@@ -28,6 +28,7 @@
     [super viewDidLoad];
     
     _content = @"《民调局异闻录》全集(1-6部全)【精校版】\r\n\r\n作者：耳东水寿(陈涛)\r\n\r\n【由Zei8.com贼吧网[Zei8.com 贼吧电子书]整理，版权归作者或出版社所有，本站仅提供预览，如侵犯您的权益，请联系本站删除。】\r\n\r\n《民调局异闻录第一部：苗乡巫祖》\r\n\r\n内容简介\r\n\r\n一九八七年，大火后的大兴安岭发现一具长着獠牙的活焦尸，解放军官兵在付出巨大代价后才将其制服，由沈辣的三叔沈援朝负责押送回北京。运送途中，焦尸再次复活，危急之时，一名神秘白发人出现，轻松便把复活的焦尸消灭掉。\r\n\r\n十几年后，天生阴阳眼的沈辣参军，被选入特种部队。在一次随队追剿毒枭的任务中，误入云南边境的一个神秘山洞；山洞内远古祭祀干尸纷纷复活，向沈辣小队发动疯狂攻击。这时，神秘白发人再次出现，将沈辣等人救出。\r\n";
+    // lalall
     
     [self prefersStatusBarHidden];
     [self.view setBackgroundColor:[ReadConfig shareInstance].theme];
@@ -38,6 +39,10 @@
         tap.delegate = self;
         tap;
     })];
+    
+    // 1223124142jfsjf
+    
+    // 看；方式开发；拉克丝；开发
     // Do any additional setup after loading the view, typically from a nib.
 }
 
